3888f13a215477cd80848fedd6887cb236352d72
HasibulRupok <rhrupok34@gmail.com>
2022-03-14 22:54:24 +0600

    all in one

---
+++ JS/weather.js
@@ -0,0 +1,45 @@
+const APIkey = "8c237ba29200c729f077bb06de2b0947";
+const limit = 1;
+
+document.getElementById("searchButton").addEventListener("click", function () {
+    const location = document.getElementById("inputField").value;
+    document.getElementById("inputField").value = "";
+    if (location == "") {
+        document.getElementById("errorDiv").style.visibility = "visible";
+        return;
+    }
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${limit}&appid=${APIkey}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => loadWeatherInfo(data[0].lat, data[0].lon, data[0].name))
+        .catch(error => {
+            document.getElementById("errorDiv").style.visibility = "visible";
+            return;
+        })
+});
+
+const loadWeatherInfo = async (lat, lon, city) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+        showWeatherInfo(data, city);
+    }
+    catch (error) {
+        console.log("Can not load the weather info");
+        console.log(error);
+    }
+
+}
+
+const showWeatherInfo = (weather, city) => {
+    document.getElementById("errorDiv").style.visibility = "hidden";
+    const weatherContainer = document.getElementById("weatherContainer");
+    const kelvin = weather.main.temp - 273.15;
+    weatherContainer.innerHTML = `
+        <h1>${city}</h1>
+        <h3>${kelvin.toFixed(2)}</h3>
+        <p>${weather.weather[0].main}</p>
+    `;
+}
